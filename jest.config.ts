4d496c3a1fbda5f7b796eb4d48b9e512c103ea11
Kishore Atapaka <atapakakishore@gmail.com>
2026-04-16 00:49:48 +1000

    test: exclude E2E tests from Jest and ignore Playwright report folder

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/e2e/',
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
